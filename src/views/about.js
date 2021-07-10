9a3160b3700324b969e0516bae2f1cd7d1bb5302
Ibrahim Waheed <Ibrahimwaheed@hotmail.co.uk>
2021-07-10 08:46:24 +0100

    fixes the spell errors

--- src/views/about.js
+++ src/views/about.js
@@ -25,16 +25,16 @@ return (
              <Divider orientation="left"><h3>About Me</h3></Divider>
               <h2>Who Am I</h2>
             <p> I am a computer science grad from Coventry university. I love developing and expanding my own programming skills 
-            and am often amazed on the ever growing of programming lanaguages. </p>
-                 <p> Click here to download my CV/Resume  currently is short as i have recently graduate😅 </p>
+            and am often amazed on the ever growing of programming languages. </p>
+                 <p> Click here to download my CV/Resume  currently is short as i have recently graduated 😅 </p>
             <Divider ><h3>What I Do </h3></Divider>
             <h2>2 year Career Plan</h2>
      
 <Timeline mode="alternate">
     <Timeline.Item color="green">Graduate Coventry University 2021 July</Timeline.Item>
-    <Timeline.Item color="green">Write Up CV and Build Up Github Repo so that it work Ready 2021 July to Augest</Timeline.Item>
+    <Timeline.Item color="green">Write Up CV and Build Up Github Repo so that it work Ready 2021 July to August</Timeline.Item>
     <Timeline.Item color="red">
-    Apply for a Graduate Role in Software development 2021 Augest to Decemeber
+    Apply for a Graduate Role in Software development 2021 August to December
     </Timeline.Item>
     <Timeline.Item color="red">Create web project on the side </Timeline.Item>
     <Timeline.Item>TBD</Timeline.Item>
